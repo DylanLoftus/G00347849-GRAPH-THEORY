67dad75ba4d3a9a0168edcfa047d39ccb76e8e10
DylanL710 <G00347849@gmit.ie>
2019-03-18 18:18:21 +0000

    add compile method to handle the stacks

--- GraphTheoryProject.py
+++ GraphTheoryProject.py
@@ -46,3 +46,56 @@ class nfa:
     def __init__(self, initial, accept):
         self.initial = initial
         self.accept = accept
+
+def compile(pofix):
+    
+    nfastack = []
+
+    for c in pofix:
+        if c == '.':
+           
+            nfa2 = nfastack.pop()
+            nfa1 = nfastack.pop()
+            
+            nfa1.accept.edge1 = nfa2.initial
+           
+            nfastack.append(nfa(nfa1.initial, nfa2.accept))
+        elif c == '|':
+            
+            nfa2 = nfastack.pop()
+            nfa1 = nfastack.pop()
+          
+            initial = state()
+            initial.edge1 = nfa1.initial
+            initial.edge2 = nfa2.initial
+            
+            accept = state()
+            nfa1.accept.edge1 = accept
+            nfa2.accept.edge1 = accept
+           
+            nfastack.append(nfa(initial, accept))
+        elif c == '*':
+            
+            nfa1 = nfastack.pop()
+          
+            initial = state()
+            accept = state()
+      
+            initial.edge1 = nfa1.initial
+            initial.edge2 = accept
+   
+            nfa1.accept.edge1 = nfa1.initial
+            nfa1.accept.edge2 = accept
+     
+            nfastack.append(nfa(initial, accept))
+        else:
+   
+            accept = state()
+            initial = state()
+
+            initial.label = c
+            initial.edge1 = accept
+
+            nfastack.append(nfa(initial, accept))
+
+    return nfastack.pop()
