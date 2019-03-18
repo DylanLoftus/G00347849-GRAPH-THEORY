a5d8769f4abf10fef247de161025f77d834d12bc
DylanL710 <G00347849@gmit.ie>
2019-03-18 18:09:36 +0000

    adding varibles to shunt method

---
+++ GraphTheoryProject.py
@@ -0,0 +1,14 @@
+# Dylan Loftus
+# Graph Theory Project 2019
+
+# Creating Shunting Yard Algorithm
+
+def shunt(infix):
+
+    specials = {'*': 50, '.': 40, '|': 30}
+
+    stack = ""
+    pofix = ""
+    
+
+    return pofix
