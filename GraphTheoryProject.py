b58caed4790a7ef16d83360b4af0cb81942b4622
DylanL710 <G00347849@gmit.ie>
2019-03-23 01:10:52 +0000

    Adding comments

--- GraphTheoryProject.py
+++ GraphTheoryProject.py
@@ -5,50 +5,66 @@
 
 def shunt(infix):
 
+    # List of special characters and their precedence.
     specials = {'*': 50, '.': 40, '|': 30}
 
+    # Empty stack
     stack = ""
+
+    # Empty pofix string, will eventually be the infix
+    # converted into postfix
     pofix = ""
     
+    # For loop loops over the infix string
     for c in infix:
+        # If we encounter a ( we add it to the stack.
         if c == '(':
             stack = stack + c
+        # If we encounter a ) we take everything off of the stack 
+        # and add it to the postfix string.
         elif c == ')':
             while stack[-1] != '(':
                 pofix = pofix + stack[-1]
                 stack = stack[:-1]
             stack = stack[:-1]
         elif  c in specials:
+        # If we encounter a special character. We check the stack to see if there
+        # are any other special characters in the stack that have higher precedence
+        # than the currently encountered special character. If there is, add it to the
+        # postfix string and take it off of the stack. If there is not add it to the stack.
             while stack and specials.get(c, 0) <= specials.get(stack[-1], 0):
                 pofix = pofix + stack[-1]
                 stack = stack[:-1]
             stack = stack + c
+        # If we encounter anything else add it to the postfix string.
         else:
             pofix = pofix + c
-
+    # Anything left over in the stack is added to the postfix string.
     while stack:
         pofix = pofix + stack[-1]
         stack = stack[:-1]
 
     return pofix
 
-
+# Class defining a state
 class state:
     label = None
     edge1 = None
     edge2 = None
 
-
+# Class defining a nfa
 class nfa:
     initial = None
     accept = None
 
+    # Constructor
     def __init__(self, initial, accept):
         self.initial = initial
         self.accept = accept
 
 def compile(pofix):
     
+    # Array for nfastack.
     nfastack = []
 
     for c in pofix:
